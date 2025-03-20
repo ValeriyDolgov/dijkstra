63c5827593956b898b1d66f6a4ed1ff17506d244
ValeriyDolgov <dolgov.valerka@mail.ru>
2025-03-20 11:51:09 +0500

    Build v1

---
+++ src/main/java/com/example/dijkstra/controller/RouteController.java
@@ -0,0 +1,27 @@
+package com.example.dijkstra.controller;
+
+import com.example.dijkstra.service.GeoJsonGraphBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/routes")
+public class RouteController {
+    private final GeoJsonGraphBuilder graphBuilder;
+
+    public RouteController() throws IOException {
+        graphBuilder = new GeoJsonGraphBuilder();
+        graphBuilder.loadGeoJson("src/main/resources/roads.geojson");
+    }
+
+    @GetMapping
+    public List<double[]> getRoute(@RequestParam double startLat, @RequestParam double startLon,
+                                 @RequestParam double endLat, @RequestParam double endLon) {
+        return graphBuilder.findShortestPath(new double[]{startLat, startLon}, new double[]{endLat, endLon});
+    }
+}
